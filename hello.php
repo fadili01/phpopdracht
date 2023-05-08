50cae36b0b3521cc32e0274aa933a0a224876339
MOHAMED FADILI <2166129@talnet.nl>
2023-05-08 10:31:29 +0200

    commit 1

---
+++ hello.php
@@ -0,0 +1,22 @@
+<?php
+// $var1 = 12;
+// $var2 = 10;
+// $sum = $var1 + $var2;
+// echo "De som van " . $var1 . " en " . $var2 . " is " . $sum;
+$var1 = 12;
+$va42 = 10;
+$resultaat = $var1 * $var2;
+echo "de resultaat van" . $var1 . "en" . $var2 . "is" . $resultaat;
+
+$naam = "mohammed";
+echo "Welkom " . $naam . "!";
+
+
+$mijnNaam = "Carl";
+$jouwNaam = "Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+?>
+
+
+
+
